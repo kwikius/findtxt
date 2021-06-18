561ce536eea3222d9a2c02aa678aa81f5afb6189
Andy Little <kwikius@yahoo.com>
2021-06-18 11:23:27 +0100

    exclude directories. add invert option

--- main.cpp
+++ main.cpp
@@ -7,10 +7,14 @@
 #include <quan/fs/get_file_dir.hpp>
 #include <quan/fs/get_wkg_dir.hpp>
 
+/*
+TODO. Could use extended-regex with grep -E or egrep
+*/
+
 int main(int argc, const char * argv[])
 {
      if(argc <2) {
-				std::cout << "useage : findtxt text [ comma-separated-extensions-list ] \n";
+				std::cout << "useage : findtxt text [ comma-separated-extensions-list ] [ not ] \n";
            return 0;
       }
 
@@ -21,31 +25,46 @@ int main(int argc, const char * argv[])
 		  char* parse = new char [len];
 		  strcpy (parse,argv[2]);
 		  const char delims[] = ", \t";
-		  
+
 		  const char* ptr = strtok(parse,delims);
 		  int count = 0;
 		  while(ptr !=0) {
 			 std::string str2 = ptr;
 			 if(count == 0){
-				 extensions = " \\( -name '" + str2 + "'";
+             // -type f -> exclude directories
+				 extensions = " \\( -type f -name '" + str2 + "'";
 			 }
 			 else {
 				 extensions += " -o -name '"  + str2 + "'";
 			 }
 			 ++count;
-			 ptr =  strtok(NULL,delims);
+			 ptr = strtok(NULL,delims);
 		  }
 		  extensions += " \\)";
         delete [] parse;
      }else {
      		extensions = " *";
-     }   
-     std::string cmd = "find " +  quan::fs::get_wkg_dir() + extensions + " -exec grep -H -n '" + std::string (argv[1]) + "' {} \\;" ;
+     }
+     std::string invert = "";
+     std::string notstr = "";
+     if ( argc >= 4){
+      if (!strcmp(argv[3],"not")){
 
-     std::cout << "find \""  << argv[1] << "\" in  \"" << argv[2] << "\"" <<std::endl;
+         invert = "--files-without-match";
+         notstr = " ! " ;
+      }
+     }
 
-     system(cmd.c_str());
-     
+     std::string cmd = "find " + quan::fs::get_wkg_dir() + extensions + " -exec egrep "
+         + invert + " -H -n '" + std::string (argv[1]) + "' {} \\;" ;
+
+     std::cout << "find \"" << notstr << argv[1] << "\" in  \"" << argv[2] << "\"" << std::endl;
+
+    if (system(cmd.c_str())){
+      std::cout << "--------------------------------------------\n";
+    }
+
 }
-   // std::string text = "find \( -name '*.pde' -o -name '*.h' -o -name '*.cpp' \) -exec grep -H -n 'elevon1_trim' {} \;"
+   // std::string text = "find \( -type f -name '*.pde' -o -name '*.h' -o -name '*.cpp' \) -exec grep -H -n 'elevon1_trim' {} \;"
    //
+
